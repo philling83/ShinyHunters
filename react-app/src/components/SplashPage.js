186fb2e31706e3fe35865f92f0ae70b63dea5b84
Philip <philipling83@gmail.com>
2021-01-28 19:20:49 -0500

    Implement onClick animation rendering of auth buttons

--- react-app/src/components/SplashPage.js
+++ react-app/src/components/SplashPage.js
@@ -1,6 +1,6 @@
 import { requirePropFactory } from '@material-ui/core';
 import React, { useState } from 'react';
-// import { NavLink } from 'react-router-dom';
+import ReactDOM from 'react-dom';
 import Modal from '@material-ui/core/Modal';
 import LoginForm from "./auth/LoginForm"
 import SignUpForm from "./auth/SignUpForm"
@@ -8,10 +8,20 @@ import LogoutButton from './auth/LogoutButton';
 import Button from '@material-ui/core/Button';
 import "./SplashPage.css"
 
+import styled, { keyframes } from 'styled-components';
+import { fadeInDown } from 'react-animations';
+
+const fadeInDownAnimation = keyframes`${fadeInDown}`;
+
+const FadeInDownDiv = styled.div`
+  animation: 3s ${fadeInDownAnimation};
+`;
+
 const Splash = ({ authenticated, setAuthenticated }) => {
 
   const [openLogin, setLoginOpen] = useState(false);
   const [openSignUp, setSignupOpen] = useState(false);
+  // const [fadeDiv, setFadeDiv] = useState(false);
 
   const handleLoginOpen = () => {
     setLoginOpen(true);
@@ -29,22 +39,63 @@ const Splash = ({ authenticated, setAuthenticated }) => {
     setSignupOpen(false);
   };
 
+  const sayHello = () => {
+    let element = (
+      <FadeInDownDiv>
+        <div className="splash_auth-buttons">
+          <div className="splash_login-button">
+            <Button variant="outlined" color="primary" onClick={handleLoginOpen}>
+              Continue
+            </Button>
+          </div>
+          <div className="splash_signup-button">
+            <Button variant="outlined" color="primary" onClick={handleSignupOpen}>
+              New Adventure
+            </Button>
+          </div>
+        </div>
+      </FadeInDownDiv>
+    )
+    ReactDOM.render(element, document.getElementById('splash_auth-container'))
+    // <FadeInDownDiv>
+      // <div className="splash_auth-buttons">
+      //   <div className="splash_login-button">
+      //     <Button variant="outlined" color="primary" onClick={handleLoginOpen}>
+      //       Continue
+      //     </Button>
+      //   </div>
+      //   <div className="splash_signup-button">
+      //     <Button variant="outlined" color="primary" onClick={handleSignupOpen}>
+      //       New Adventure
+      //     </Button>
+      //   </div>
+      // </div>
+    // </FadeInDownDiv>
+    // )
+    // setFadeDiv(true)
+  }
+
+
   return (
     <>
+    <Button onClick={sayHello}>Yo</Button>
     <div className="splash-container">
       <img className="splash-container_image" src={require("../Assets/Pokedex.png")} alt="" />
-      <div className="splash_auth-buttons">
-        <div className="splash_login-button">
-          <Button variant="outlined" color="primary" onClick={handleLoginOpen}>
-            Continue
-          </Button>
+      <div id='splash_auth-container' />
+      {/* <FadeInDownDiv>
+        <div className="splash_auth-buttons">
+          <div className="splash_login-button">
+            <Button variant="outlined" color="primary" onClick={handleLoginOpen}>
+              Continue
+            </Button>
+          </div>
+          <div className="splash_signup-button">
+            <Button variant="outlined" color="primary" onClick={handleSignupOpen}>
+              New Adventure
+            </Button>
+          </div>
         </div>
-        <div className="splash_signup-button">
-          <Button variant="outlined" color="primary" onClick={handleSignupOpen}>
-            New Adventure
-          </Button>
-        </div>
-      </div>
+      </FadeInDownDiv> */}
     </div>
     <Modal
       open={openLogin}
